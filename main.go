2c48c363e9bb799af52b2847e8fe490dbf45b2e9
vicky <tyagivivek528@gmail.com>
2023-10-06 12:02:23 +0530

    Crud application is done

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"math/rand"
 	"encoding/json" //This library to render the data into json to send the data request
 	"fmt"           //Basic formatting methods to get the stuff in the terminal
 	"log"           //To log the data on the frontend of the application basically in the black screen
 	"net/http"      //To import the webserver facility
+	"strconv"
 
 	//To generate a random number
 	//To convert the datatypes "from" and "to" to the string represenation
@@ -26,8 +28,15 @@ director *Director `json:"director"`
 
 var movies []Movie//For now basically this is the storage we're using...and initially we're going to add some of the movies in it
 
-func getMovie(){
+func getMovie(w http.ResponseWriter, r *http.Request){
+   w.Header().Set("Content-Type", "application/json")
+   params:=mux.Vars(r)
+   for _, item := range movies{
+ if item.id == params[":id"]{
+  json.NewEncoder(w).Encode(item)
+ }
 
+}
 }
 
 func getMovies(w http.ResponseWriter, r *http.Request){
@@ -48,9 +57,31 @@ break
 
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request){
+w.Header().Set("Content-Type", "application/json")
+var movie Movie
+_ =json.NewDecoder(r.Body).Decode(&movie) //To decode the json request into struct
+movie.id = strconv.Itoa(rand.Intn(1000000))
+movies = append(movies, movie)
+json.NewEncoder(w).Encode(movie) //Used to encode the struct in golang to the json
+}
 
+func updateMovie(w http.ResponseWriter, r *http.Request){
+w.Header().Set("Content-Type", "application/json")
+params:=mux.Vars(r)
 
+for index, item := range movies {
+if item.id == params["id"]{
+movies = append(movies[:index], movies[index+1:]...)
 
+var movie Movie
+_ = json.NewDecoder(r.Body).Decode(&movie)
+movie.id = params["id"]
+movies = append(movies, movie)
+json.NewEncoder(w).Encode(movie)
+}
+}
+}
 
 func main(){
 r:=mux.NewRouter() //returns a new router instance
